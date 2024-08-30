58fdcc9bc75c97959730ce114e790dcd9f59bf62
tone <3341154833@qq.com>
2024-08-30 12:54:22 +0800

    BlogList接口返回访问量及点赞量

--- Server/src/APIs/GetBlogList.ts
+++ Server/src/APIs/GetBlogList.ts
@@ -10,7 +10,7 @@ class GetBlogList extends API {
     private defaultAccessLevel = 6;
 
     public async onRequset(data: any, res: any) {
-        let blogListRes = await MySQLConnection.execute('SELECT uuid, title, description, publish_time from blog WHERE access_level > ? ORDER BY publish_time DESC',[this.defaultAccessLevel]);
+        let blogListRes = await MySQLConnection.execute('SELECT uuid, title, description, publish_time, visit_count, like_count from blog WHERE access_level > ? ORDER BY publish_time DESC',[this.defaultAccessLevel]);
         if(!blogListRes){
             this.logger.error('查询时数据库发生错误');
             return res.json(ServerStdResponse.SERVER_ERROR);
