42cd5015a8240d96c3edb41c1b7e08ff664af6a0
tone <3341154833@qq.com>
2024-09-06 11:58:24 +0800

    为API抽象类添加注解和类型限定

--- Server/src/Plugs/API/API.ts
+++ Server/src/Plugs/API/API.ts
@@ -1,11 +1,22 @@
+import { Request, Response, NextFunction } from "express";
 import Logger from "../Logger";
+
+interface MiddlewareFunction {
+    (req: Request, res: Response, next: NextFunction): void;
+}
 
 abstract class API {
 
     protected logger: Logger;
     public middlewareFunc: Function[] = [];
-    constructor(public method: string, public uri: string, ...func: any) {
-        this.logger = new Logger('API][' + method + '][' + uri);
+
+    /**
+     * @param method API方法
+     * @param path API路由Path
+     * @param func API中间件函数
+     */
+    constructor(public method: string, public path: string, ...func: MiddlewareFunction[]) {
+        this.logger = new Logger('API][' + method + '][' + path);
         this.middlewareFunc.push(...func);
     }
 
