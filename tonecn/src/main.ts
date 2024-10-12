cf29c352666b6ad4858a7afc567422550781ecbe
tone <3341154833@qq.com>
2024-10-12 11:02:49 +0800

    添加 ElementPlus深色模式

--- tonecn/src/main.ts
+++ tonecn/src/main.ts
@@ -3,6 +3,7 @@ import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 import App from './App.vue'
 import router from './router'
