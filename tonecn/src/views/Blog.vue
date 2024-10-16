8e6f25d8b39a2db0ff89aa29b795ebe1d8370c41
tone <3341154833@qq.com>
2024-10-16 22:10:01 +0800

    添加 前端博客列表加密文章提示

--- tonecn/src/views/Blog.vue
+++ tonecn/src/views/Blog.vue
@@ -29,7 +29,10 @@ onMounted(async () => {
             <div class="text-center mt-[20px] mb-[300px] dark:text-white" v-if="loadStatus == 0">加载中，请稍后...</div>
             <el-empty description="暂无数据" style="margin: 0 auto;" v-if="loadStatus == 1 && blogList.length == 0" />
             <div class="mx-auto max-w-[400px] w-full flex flex-col" v-for="item of blogList">
-                <a class="text-[26px] font-semibold cursor-pointer block hover:underline dark:text-white text-[#333]" :href="`/blogContent/${item.uuid}`" target="_blank">{{ item.title }}</a>
+                <a class="text-[26px] font-semibold cursor-pointer block hover:underline dark:text-white text-[#333]" :href="`/blogContent/${item.uuid}`" target="_blank">
+                    {{ item.title }}
+                    <span v-if="item.access_level == 9" class="text-[10px] text-[#666] dark:text-[#ccc] font-normal border border-[#d7d9de] py-[2px] px-[4px] rounded-[10px] bg-[#ebecf0] dark:border-[#999] dark:!bg-[#ffffff22]">受密码保护</span>
+                </a>
                 <div class="text-[#666] dark:text-[#ccc]">{{ item.description }}</div>
                 <div class="text-[#888] mt-[15px] text-[14px]">{{ timestampToString(+item.publish_time) }} ——
                     {{ formateTimes(item.visit_count) }} 次访问</div>
