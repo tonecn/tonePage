c99b76e7a9c6eb197b16abbb238fddac976d2fc7
tone <3341154833@qq.com>
2025-04-24 19:37:31 +0800

    使用shadcn重构

--- tone-page-web/app/page.tsx
+++ tone-page-web/app/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-import { useState } from "react";
-
 export default function Home() {
-  const [nums, setNum] = useState({ num: 0 });
   return (
     <div>
-      Home
+
     </div>
   );
 }
