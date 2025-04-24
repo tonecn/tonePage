a55726c7246cf0e78fd170c00731ae1f5f92630e
tone <3341154833@qq.com>
2025-04-24 17:46:11 +0800

    提交nextjs新项目

---
+++ tone-page-web/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "特恩的日志",
+  description: "一名啥都会一点点的程序员",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tone-page-web/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [nums, setNum] = useState({ num: 0 });
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
