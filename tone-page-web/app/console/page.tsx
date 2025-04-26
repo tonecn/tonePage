e6107f3fbe9defa8b4148763268697c393b5f6e7
tone <3341154833@qq.com>
2025-04-26 13:00:17 +0800

    完善console界面登录样式

--- tone-page-web/app/console/page.tsx
+++ tone-page-web/app/console/page.tsx
@@ -1,10 +1,20 @@
-import { LoginForm } from "@/components/login-form";
+'use client';
+
+import { LoginForm, LoginFormData } from "@/components/login-form";
+
+const handleSubmit = async (data: LoginFormData) => {
+    console.log(data)
+}
+
+const handleSendCode = async (data: LoginFormData) => {
+    console.log(data)
+}
 
 export default function Console() {
     return (
         <div className="flex flex-1 flex-col items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm md:max-w-3xl">
-                <LoginForm />
+                <LoginForm onSubmit={handleSubmit} onSendCode={handleSendCode} />
             </div>
         </div>
     )
