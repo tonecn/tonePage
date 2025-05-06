f3258605552e4c10035e3a049fe5bdf27961769e
tone <3341154833@qq.com>
2025-05-07 00:01:32 +0800

    允许userService进行or查询

--- tone-page-server/src/user/user.service.ts
+++ tone-page-server/src/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 
+type UserFindOptions = Partial<Pick<User, 'userId' | 'username' | 'phone' | 'email'>>;
+
 @Injectable()
 export class UserService {
     constructor(
@@ -10,7 +12,7 @@ export class UserService {
         private readonly userRepository: Repository<User>,
     ) { }
 
-    async findOne(options: Partial<Pick<User, 'userId' | 'username' | 'phone' | 'email'>>): Promise<User | null> {
+    async findOne(options: UserFindOptions | UserFindOptions[]): Promise<User | null> {
         if (Object.keys(options).length === 0) {
             return null;
         }
