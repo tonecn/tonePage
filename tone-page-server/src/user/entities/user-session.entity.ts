f3243ef5d2b665941dc522151307f9ca431e8497
tone <3341154833@qq.com>
2025-05-07 13:36:26 +0800

    加入userSession实体

---
+++ tone-page-server/src/user/entities/user-session.entity.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+@Index(['sessionId', 'userId'])
+export class UserSession {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 36 })
+    sessionId: string;
+
+    @Column({ length: 36 })
+    userId: string;
+
+    @CreateDateColumn({ precision: 3 })
+    createdAt: Date;
+
+    @DeleteDateColumn({ nullable: true, precision: 3 })
+    deletedAt: Date;
+}
