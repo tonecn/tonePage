0fca14d99f24d1506715f774c9db9f5e21f5424d
tone <3341154833@qq.com>
2025-05-07 13:38:14 +0800

    加入userSession Service

---
+++ tone-page-server/src/user/services/user-session.service.ts
@@ -0,0 +1,21 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { Injectable } from "@nestjs/common";
+import { UserSession } from "../entities/user-session.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserSessionService {
+    constructor(
+        @InjectRepository(UserSession)
+        private readonly userSessionRepository: Repository<UserSession>,
+    ) { }
+
+
+    async createSession(userId: string, sessionId: string): Promise<UserSession> {
+        const session = this.userSessionRepository.create({
+            userId,
+            sessionId,
+        });
+        return await this.userSessionRepository.save(session);
+    }
+}
