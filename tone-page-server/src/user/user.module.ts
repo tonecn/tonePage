2e77530f66999cdfa73e9dc58fb3b3dbadf5f753
tone <3341154833@qq.com>
2025-05-07 13:37:27 +0800

    user模块注册userSession实体

--- tone-page-server/src/user/user.module.ts
+++ tone-page-server/src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
+import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserSession } from './entities/user-session.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User, UserSession])],
     controllers: [UserController],
     providers: [UserService],
     exports: [UserService],
