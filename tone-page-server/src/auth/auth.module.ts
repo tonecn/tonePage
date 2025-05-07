6fc4dbf57b52691c35941f92337ecce4b6b6b6c7
tone <3341154833@qq.com>
2025-05-07 13:36:51 +0800

    auth模块加入userSession和jwt

--- tone-page-server/src/auth/auth.module.ts
+++ tone-page-server/src/auth/auth.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSession } from 'src/user/entities/user-session.entity';
+import { UserSessionService } from 'src/user/services/user-session.service';
 
 @Module({
   imports: [
     UserModule,
+    TypeOrmModule.forFeature([UserSession]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'tone-page',
+      signOptions: {
+        expiresIn: process.env.EXPIRES_IN || '1d',
+      }
+    })
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserSessionService],
 })
 export class AuthModule { }
