7d34807edb88a5e533559bce5529cf455a36ffc0
tone <3341154833@qq.com>
2025-05-13 11:34:37 +0800

    添加图片oss域

--- tone-page-web/next.config.ts
+++ tone-page-web/next.config.ts
@@ -14,6 +14,9 @@ const nextConfig: NextConfig = {
       }
     ]
   },
+  images: {
+    remotePatterns: [new URL('https://tone-personal.oss-cn-chengdu.aliyuncs.com/**')]
+  }
 };
 
 export default nextConfig;
