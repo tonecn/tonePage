bd0cba2526d2d4d31fd74869f712d7d6db104c9f
tone <3341154833@qq.com>
2025-05-18 14:29:50 +0800

    添加base62编码工具

--- tone-page-web/lib/utils.ts
+++ tone-page-web/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import basex from "base-x"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const base62 = basex('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
