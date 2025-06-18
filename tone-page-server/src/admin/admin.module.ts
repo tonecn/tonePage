a5b8fa49edbd1e9845db7515c1b704e24d43b91e
tone <3341154833@qq.com>
2025-06-18 16:23:08 +0800

    彻底移除权限模块。。。

--- tone-page-server/src/admin/admin.module.ts
+++ tone-page-server/src/admin/admin.module.ts
@@ -4,7 +4,6 @@ import { AdminUserController } from './controller/admin-user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { UserModule } from 'src/user/user.module';
-import { RoleModule } from 'src/role/role.module';
 import { AdminWebResourceController } from './controller/web/admin-web-resource.controller';
 import { AdminWebBlogController } from './controller/web/admin-web-blog.controller';
 import { ResourceModule } from 'src/resource/resource.module';
@@ -14,7 +13,6 @@ import { BlogModule } from 'src/blog/blog.module';
   imports: [
     TypeOrmModule.forFeature([User]),
     UserModule,
-    RoleModule,
     ResourceModule,
     BlogModule,
   ],
