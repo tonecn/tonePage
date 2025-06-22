f3193226e7ceeea03ada49953b5352a07fcac3b6
tone <3341154833@qq.com>
2025-06-22 23:25:38 +0800

    调整数据库结构、添加博客权限enum

---
+++ tone-page-server/src/blog/blog.permission.enum.ts
@@ -0,0 +1,5 @@
+export enum BlogPermission {
+    Public = 'Public',
+    ByPassword = 'ByPassword',
+    List = 'List',
+}
--- tone-page-server/src/blog/entity/Blog.entity.ts
+++ tone-page-server/src/blog/entity/Blog.entity.ts
@@ -8,6 +8,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { BlogComment } from './BlogComment.entity';
+import { BlogPermission } from '../Blog.Permission.enum';
 
 @Entity()
 export class Blog {
@@ -35,7 +36,12 @@ export class Blog {
   @DeleteDateColumn({ precision: 3, nullable: true })
   deletedAt: Date;
 
-  // 权限关系 TODO
+  // 权限
+  @Column('simple-array', { default: '' })
+  permissions: BlogPermission[];
+
+  @Column({ nullable: true })
+  password_hash: string | null;
 
   // 关系
   @OneToMany(() => BlogComment, (blog) => blog.id)
