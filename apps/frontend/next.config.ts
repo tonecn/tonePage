7d3a809fa73fb28ba348fed6bb6901dc360faebf
tone <tonesc@qq.com>
2025-12-12 20:44:33 +0800

    fix: 修复常规请求api路径缺失问题

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/:path*',
+        destination: 'http://localhost:3001/api/:path*',
       }
     ]
   },
