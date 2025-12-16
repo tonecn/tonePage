b235ca8a6ee0160d5bf255a9ec6b78883f78e195
tone <tonesc@qq.com>
2025-12-16 22:48:07 +0800

    feat: 后端加入cookie-parser

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { GlobalExceptionsFilter } from './common/filters/global.exceptions.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
