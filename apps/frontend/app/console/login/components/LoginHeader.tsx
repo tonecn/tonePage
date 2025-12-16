12c84f3dc825d570c2781f0116f145f6d8cf8764
tone <tonesc@qq.com>
2025-12-16 22:49:31 +0800

    优化LoginHeader组件

--- apps/frontend/app/console/login/components/LoginHeader.tsx
+++ apps/frontend/app/console/login/components/LoginHeader.tsx
@@ -1,10 +1,15 @@
-export default function LoginHeader() {
+interface LoginHeaderProps {
+    h1?: string;
+    h2?: string;
+}
+
+export default function LoginHeader({ h1, h2 }: LoginHeaderProps) {
     return (
         <>
             <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">欢迎回来</h1>
+                <h1 className="text-2xl font-bold">{h1 || '欢迎回来'}</h1>
                 <p className="text-muted-foreground text-balance">
-                    登陆到您的账户
+                    {h2 || '登陆到您的账户'}
                 </p>
             </div>
         </>
