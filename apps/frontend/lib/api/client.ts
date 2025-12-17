e6fad12b30d8953450bf1ac3666fa2dcc9f8bccc
tone <tonesc@qq.com>
2025-12-17 23:01:45 +0800

    feat: 前端实现登陆短信验证发送

--- apps/frontend/lib/api/client.ts
+++ apps/frontend/lib/api/client.ts
@@ -47,3 +47,4 @@ export async function clientFetch<T = unknown>(
 
 export * as AuthAPI from './endpoints/auth.client'
 export * as UserAPI from './endpoints/user.client'
+export * as SmsAPI from './endpoints/sms.client'
