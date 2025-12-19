a960a8b07ff367d45e9746ef3113b78bf33c7988
tone <tonesc@qq.com>
2025-12-19 10:23:34 +0800

    chore: 暂时移除use-oss-sts的API

--- apps/frontend/hooks/oss/use-oss-sts.ts
+++ apps/frontend/hooks/oss/use-oss-sts.ts
@@ -1,11 +1,11 @@
-import { OssApi } from "@/lib/api";
+// import { OssApi } from "@/lib/api";
 import { toast } from "sonner";
 import useSWR from "swr";
 
 export function useOssSts() {
     const { data: stsTokenData, isLoading, error, mutate } = useSWR(
         '/api/oss/sts',
-        () => OssApi.getStsToken(),
+        // () => OssApi.getStsToken(),
         {
             shouldRetryOnError: false,
             // refreshInterval: 59 * 60 * 1000,
